b4a6472be0f276d24461a675d393fbf1326fb707
MalkingMan <arrayalkhozini@gmail.com>
2025-10-30 09:23:32 +0700

    Menambahkan `OutlinedTextField` untuk alamat.

--- app/src/main/java/com/example/textedit/TeksEdit.kt
+++ app/src/main/java/com/example/textedit/TeksEdit.kt
@@ -1,12 +1,24 @@
 package com.example.textedit
 
-import android.R
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -51,7 +63,7 @@ Fun FormDataDiri(modifier : Modifier
                     selected = textJK == item,
                     onClick = {textJK = item}
                 )
-        })
+        }
             Text(text = item)
         }
     }
@@ -64,6 +76,17 @@ Fun FormDataDiri(modifier : Modifier
         onValueChange = {
             textAlamat = it
         }
+    )
+
+    HorizontalDivider(
+        modifier = Modifier.padding(button = dimensionResource(30dp),top = dimensionResource(
+            id = 30dp
+        )),
+        thickness = dimensionResource(id = 1.dp)
+    )
+
+
+
 
 
 
