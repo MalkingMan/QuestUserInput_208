9e32cc1a7cf6cf186fc1fec3960f2de48d8d62d0
MalkingMan <arrayalkhozini@gmail.com>
2025-10-30 08:53:57 +0700

    Menambahkan data form data diri

---
+++ app/src/main/java/com/example/textedit/TeksEdit.kt
@@ -0,0 +1,28 @@
+package com.example.textedit
+
+import android.R
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import ....
+
+@Composable
+
+Fun FormDataDiri(modifier : Modifier
+){
+    var textNama by remember { mutableStateOf(value ="") }
+    var textAlamat by remember { mutableStateOf(value="") }
+    var textJK by remember { mutableStateOf(value="") } }
+
+    var nama by remember { mutableStateOf(value ="") }
+    var alamat by remember { mutableStateOf(value="") }
+    var jenis by remember { mutableStateOf(value ="") }
+
+    val gender:List<string> = ListOf("laki-Laki","Perempuan")
+
+    Column(modifier = modifier.padding(top 50,dp),
+        verticalArrangement = Arrangement.Top,
+        horizontal
+
+    )
+}
+
